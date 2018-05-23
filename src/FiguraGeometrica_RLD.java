b4afdc43f5710fc866519b7cbcf13c3aacffb3a7
Ruben <rubenldsmr@gmail.com>
2018-05-23 12:00:29 +0200

    Version refactorizada

--- src/FiguraGeometrica_RLD.java
+++ src/FiguraGeometrica_RLD.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_RLD {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_RLD(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
